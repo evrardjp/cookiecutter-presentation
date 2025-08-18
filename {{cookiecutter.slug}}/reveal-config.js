b0a1bc1294ad789989d1d7345f37b7450990adff
Florian Haas <xahteiwi@mailbox.org>
2025-08-18 17:26:29 +0200

    Default to reveal.js 5.2.1, drop support for 4.x
    Using version 5 should be a safe bet by now.

--- {{cookiecutter.slug}}/reveal-config.js
+++ {{cookiecutter.slug}}/reveal-config.js
@@ -23,11 +23,7 @@ Reveal.initialize({
 {% if cookiecutter.reveal_js_menu_version != 'disable' %}
     menu: {
         themes: {{ cookiecutter.menu_enable_theme_switch | default(true) | lower }},
-{% if cookiecutter.reveal_js_version | first | int >= 4 %}
         themesPath: 'reveal.js/dist/theme',
-{% else %}
-        themesPath: 'reveal.js/css/theme',
-{% endif %}
         transitions: false,
         openButton: false,
         openSlideNumber: true,
@@ -75,35 +71,12 @@ Reveal.initialize({
     },
 
 {% endif %}
-{% if cookiecutter.reveal_js_version | first | int < 4 %}
-    // Optional libraries used to extend on reveal.js
-    dependencies: [
-        { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-        { src: 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-        { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-        { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-{% if cookiecutter.multiplex == 'enable' %}
-        { src: '//cdn.socket.io/socket.io-1.3.5.js', async: true },
-        { src: 'reveal.js/plugin/multiplex/client.js', async: true },
-{% endif %}
-{% if cookiecutter.reveal_js_menu_version != 'disable' %}
-        { src: 'reveal.js-menu/menu.js', async: true, condition: function() { return !!document.body.classList; } },
-{% endif %}
-{% if cookiecutter.charts == 'enable' %}
-        { src: '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js' },
-        { src: 'reveal.js-plugins/chart/csv2chart.js' },
-{% endif %}
-    ]
-{% else %}
 {% if cookiecutter.multiplex == 'enable' %}
     dependencies: [
        { src: '{{ cookiecutter.multiplex_url }}/socket.io/socket.io.js', async: true },
        { src: '{{ cookiecutter.multiplex_url }}/client.js', async: true }
     ],
 {% endif %}
-
     plugins: [
 	RevealMarkdown,
 	RevealNotes,
@@ -111,5 +84,4 @@ Reveal.initialize({
         RevealZoom{% if cookiecutter.reveal_js_menu_version != 'disable' %},
         RevealMenu{% endif %}
     ]
-{% endif %}
 });
